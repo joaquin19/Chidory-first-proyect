40360c1b9347821a6f17a928da76220ccf6a47b0
Joaquin Navedo <JoaquinNavedo@laMBPdeJoaquin.www.towasoftware.com>
2018-03-09 12:58:20 -0600

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,53 @@
+// Modulos
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
-
+// Bootstrap
 import { AppComponent } from './app.component';
+
+// Componentes
+import { LoginComponent } from './login/login.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { RegisterComponent } from './login/register.component';
+
+// Servicios ò Provideers
+import { SharedService } from './services/shared.service';
+import { SidebarService } from './services/sidebar.service';
+
+// Ruta
+import {APP_ROUTING} from './app.routes';
+import { PagesComponent } from './pages/pages.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NopagefoundComponent,
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent,
+    PagesComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING,
+    FormsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [SharedService, SidebarService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
